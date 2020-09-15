2a4444730401f11f09f70cd1f8220ea916343096
grecha505@gmail.com <droppot_1342>
2020-09-16 00:01:35 +0300

    update links

--- test_main_page.py
+++ test_main_page.py
@@ -2,14 +2,15 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # init Main Page object with params
     page.open()  # inherited function from Base Page
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # init Main Page object with params
     page.open()  # inherited function from Base Page
     page.should_be_login_link()
+
