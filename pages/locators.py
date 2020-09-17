2c565707fe0d77dfd72968e93eee480f86f10c5b
grecha505@gmail.com <droppot_1342>
2020-09-17 22:19:33 +0300

    add ProductPageLocators, 4-3-2

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators:
     LOGIN_TAG_IN_URL = "/login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME_FROM_ALERT = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
+    PRODUCT_PRICE_FROM_ALERT = (By.CSS_SELECTOR, "#messages .alert-info strong")
