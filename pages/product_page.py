0e72216f170b7143707846c6c148bc9d78189920
grecha505@gmail.com <droppot_1342>
2020-09-18 19:23:05 +0300

    4-3-6, 4-3-9, new asserts

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,7 +6,7 @@ class ProductPage(BasePage):
 
     def add_prod_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), \
-            "Button for add to basket not found!"
+               "Button for add to basket not found!"
         self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET).click()
 
     def should_be_correct_name(self):
@@ -18,3 +18,11 @@ class ProductPage(BasePage):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text == \
                self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_FROM_ALERT).text, \
                "Price in basket not correct!"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ALERT_SUCCESS), \
+               "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.ALERT_SUCCESS), \
+               "Success message not disappeared, but should be"
