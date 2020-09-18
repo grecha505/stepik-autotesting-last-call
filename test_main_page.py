52a9dd26a670ab4320b97eb975c880e943a1db60
grecha505@gmail.com <droppot_1342>
2020-09-18 20:31:18 +0300

    4-3-10, new tests for basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,9 @@
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-
+from pages.basket_page import BasketPage
+import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -17,3 +20,13 @@ def test_guest_should_see_login_link(browser):
     page.open()  # inherited function from Base Page
     page.should_be_login_link()
 
+
+@pytest.mark.step4310
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # init Main Page object with params
+    page.open()  # inherited function from Base Page
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_not_items_in_basket()
+    basket_page.should_be_text_about_empty_basket()
