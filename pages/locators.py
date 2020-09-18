a5e9deb6f74932f6b2f75c183deab1fbc00394bd
grecha505@gmail.com <droppot_1342>
2020-09-18 19:22:32 +0300

    4-3-6, 4-3-9, BasePageLocators added

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPageLocators:
+    pass
 
 
 class LoginPageLocators:
@@ -17,3 +22,4 @@ class ProductPageLocators:
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     PRODUCT_NAME_FROM_ALERT = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
     PRODUCT_PRICE_FROM_ALERT = (By.CSS_SELECTOR, "#messages .alert-info strong")
+    ALERT_SUCCESS = (By.CSS_SELECTOR, ".alert-success")
