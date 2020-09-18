f9e2f225802f2914aacf695ab7856e6a971a3958
grecha505@gmail.com <droppot_1342>
2020-09-18 20:30:52 +0300

    4-3-10, add BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, "[href=\"/ru/basket/\"]")
 
 
 class MainPageLocators:
@@ -23,3 +24,8 @@ class ProductPageLocators:
     PRODUCT_NAME_FROM_ALERT = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
     PRODUCT_PRICE_FROM_ALERT = (By.CSS_SELECTOR, "#messages .alert-info strong")
     ALERT_SUCCESS = (By.CSS_SELECTOR, ".alert-success")
+
+
+class BasketPageLocators:
+    TEXT_ABOUT_EMPTY = (By.CSS_SELECTOR, "#content_inner > p")
+    BASKET_ITEMS = (By.CSS_SELECTOR, "#basket-items")
